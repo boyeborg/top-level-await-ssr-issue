c8032c280d1b6f1c9f9fdac364750ebbcb697365
Boye Borg Nygård <6630430+boyeborg@users.noreply.github.com>
2022-06-30 15:34:34 +0200

    add top level await plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  plugins: [topLevelAwait()],
   build: {
     ssr: true,
     lib: { formats: ["es"], entry: "main.js" }
